419148059339afbccb8726888e1a398ec4ab9c0c
Alex Tereshkin <atereshkin@y-node.com>
2009-12-24 14:08:52 +0200

    Use "blur" instead of timer to htmlize links

--- linkeditor.js
+++ linkeditor.js
@@ -1,17 +1,15 @@
 (function($){
-   var regexp = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
+   var regexp = /[\n\r\s](ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
    var htmlizeLinks = function(string){
      var matches = string.match(regexp);
      if (!matches){
        return string;
      }
      var newStr = string;
-     alert(matches.length);
      for (var i=0, ln=matches.length; i<ln; i++ ){
-       var match = matches[i];
-       newStr=newStr.replace(match, '<a href="'+match+'">' + match+'</a>');
+       var match = matches[i].substring(1);
+       newStr=newStr.replace(match, '<a href="'+match+'" target="_blank">' + match+'</a>');
      }
-     alert(newStr);
      return newStr;
    };
    $.fn.linkeditor=function(){
@@ -19,12 +17,20 @@
 		 var $this = $(this);
 		 var doc = this.contentWindow.document;
 		 doc.designMode = 'on';
-		 doc.contentEditable = 'true';
 		 var $db = $(doc.body);
-		 setInterval(function(){
-		   $db.html(htmlizeLinks($db.html()));
-		 },5000);
-
+		 $(doc).blur(
+		   function(){
+		     $db.html(htmlizeLinks($db.html()));
+	   	     $db.find('a').click(function(e){
+					   window.open($(this).attr('href'));
+					   e.stopPropagation();
+					   return false;
+					 });
+		     doc.designMode = 'off';
+		   });
+		 $(doc).focus(function(){
+				doc.designMode = 'on';
+			      });
 	       });
    };
  })(jQuery);
