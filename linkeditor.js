a77aa0d83de4b0cca98ddb7f747abf30cd741e8b
Alex Tereshkin <atereshkin@y-node.com>
2009-12-24 16:46:46 +0200

    Apply to textareas Better url regexp

--- linkeditor.js
+++ linkeditor.js
@@ -1,5 +1,5 @@
 (function($){
-   var regexp = /[\n\r\s](ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
+   var regexp = /[\n\r\s](((ftp|http|https):\/\/)|www\.)(\w+:{0,1}\w*@)?[-A-Za-z0-9\.]+\.[-A-Za-z]+(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/g;
    var htmlizeLinks = function(string){
      var matches = string.match(regexp);
      if (!matches){
@@ -8,15 +8,33 @@
      var newStr = string;
      for (var i=0, ln=matches.length; i<ln; i++ ){
        var match = matches[i].substring(1);
-       newStr=newStr.replace(match, '<a href="'+match+'" target="_blank">' + match+'</a>');
+       var url;
+       if (match.search('//')>-1)
+	 url = match;
+       else
+	 url = 'http://' + match;
+       newStr=newStr.replace(match, '<a href="'+url+'" target="_blank">' + match+'</a>');
      }
      return newStr;
    };
    $.fn.linkeditor=function(){
      this.each(function(){
 		 var $this = $(this);
-		 var doc = this.contentWindow.document;
+		 var $editor = $('<iframe src="javascript:;" height="200" width="400"></iframe>').attr('id', $this.attr('id')+'_if');
+		 $this.after($editor).hide();
+
+		 $editor.css({
+			       'height' : $this.css('height'),
+			       'width' : $this.css('width')
+			     }
+		 );
+		 var doc = $editor.get(0).contentWindow.document;
 		 doc.designMode = 'on';
+		 doc.open();
+		 doc.write($this.val());
+		 doc.close();
+		 doc.contentEditable = 'true';
+
 		 var $db = $(doc.body);
 		 $(doc).blur(
 		   function(){
